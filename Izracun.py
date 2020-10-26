2a602fc57e9837730e596a250af851c4acc57210
ezvr <11723882+ezvr@users.noreply.github.com>
2020-10-26 12:02:44 +0100

    added comments

--- Izracun.py
+++ Izracun.py
@@ -1,7 +1,14 @@
 import numpy as np
 
 
-def izracun():
+def izracun(x: float = 0, y: float = 0):
+    """Izračunaj vrednost temperature pri podanih podatkih.
+
+    Keyword arguments:
+    x -- premik v x smeri [m] (default 0.0)
+    y -- premik v y smeri [m] (default 0.0)
+    """
+
     print('jjaka')
 
     q = 15  # W/m2
@@ -13,8 +20,8 @@ def izracun():
     k = 0.35  # W/mK
 
     # n=1
-    T_x_y = ((2*q*L*(1-(-1) ^ n)) / (k*(n) ^ 2 *
-                                     (np.pi) ^ 2 * np.cosh*((n*np.pi * y)/L)))
+    T_x_y = ((2*q*L*(1-(-1) ** n)) / (k*(n) ^ 2 *
+                                     (np.pi) ** 2 * np.cosh*((n*np.pi * y)/L)))
 
     rezultat = T_x_y
 
