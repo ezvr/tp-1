8eb975c4a3872e67e98b74dd19fbc926ac160673
ezvr <11723882+ezvr@users.noreply.github.com>
2020-10-26 12:57:24 +0100

    nastavek kalkulacije

--- main.py
+++ main.py
@@ -7,4 +7,19 @@ W = 0.8
 xarray = np.arange(0,L,0.1)
 yarray = np.arange(0,W,0.1)
 
-print(xarray)
+
+
+result = []
+
+for x in xarray:
+    y_ar = []
+    for y in yarray:
+        print(x,y)
+        print(Izracun.izracun(x,y))
+        y_ar.append(Izracun.izracun(x,y))
+    result.append(y_ar)
+
+
+
+for x in result:
+    print(max(x))
