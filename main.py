0c11c843c233adac1a49117ce16abdc53122cc00
ezvr <11723882+ezvr@users.noreply.github.com>
2020-10-26 16:06:05 +0100

    dodane linije

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
 import Izracun
-import numpy as np
-import matplotlib.pyplot as plt
-import os
 
 
-L = 0.71
-W = 0.71
+def entry():
+    # TREBA NARDIT DA SE LAHKO VNESE PODATKE
+    L = 0.70  # m
+    W = 0.70  # m
+    step = 0.01
 
-xarray = np.arange(0,L,0.01)
-yarray = np.arange(0,W,0.01)
-
-
-
-result = []
-
-for x in xarray:
-    y_ar = []
-    for y in yarray:
-        y_ar.append(Izracun.izracun(x,y))
-    result.append(y_ar)
+    izrisi_graf(L, W, step)
 
 
+def izrisi_graf(L: float, W: float, step: float):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import os
+
+    xarray = np.arange(0, L+step, step)
+    yarray = np.arange(0, W+step, step)
+    result = []
+
+    # Pravilno oblikovanje podatkov
+    for x in xarray:
+        y_ar = []
+        for y in yarray:
+            y_ar.append(Izracun.izracun(x, y))
+        result.append(y_ar)
+    nparray = np.array(result)
+    X, Y = np.meshgrid(xarray, yarray)
+
+    # Izrišemo graf
+    plt.subplots()
+    plt.contourf(Y, X, nparray, 15, cmap='jet')
+    plt.grid(alpha=0.2)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    cntr = plt.contour(Y, X, nparray, 15, colors='black', linewidths=.5)
+
+    plt.clabel(cntr, inline_spacing=1, fmt='%.2f', fontsize=10)
+
+    plt.savefig(dir_path+'/fig1.png')
 
 
-nparray = np.array(result)
-
-
-X, Y = np.meshgrid(xarray, yarray)
-ax = plt.contourf(Y,X, nparray,50)
-plt.grid(alpha = 0.2)
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-plt.savefig(dir_path+'/fig1.png')
-
+entry()
