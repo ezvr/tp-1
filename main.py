8f1b4ce3ddfc2b496d1671df43721684bc604207
ezvr <11723882+ezvr@users.noreply.github.com>
2020-10-27 17:06:22 +0100

    popravek kalkulacije, zdaj uporablja np meshgrid

--- main.py
+++ main.py
@@ -1,53 +1,44 @@
-import Izracun
-
-
-def entry():
-    # TREBA NARDIT DA SE LAHKO VNESE PODATKE
+def __main__():
+    # TREBA NARDIT DA SE LAHKO VNESE PODATKE ?
     podatki = {
-        'L' : 0.70,  # m
-        'W' : 0.70,  # m
+        'L' : 1,  # m
+        'W' : 1,  # m
         'q' : 15,  # W/m2
         'n': 1,  # /
         'T1' : 320,  # K
         'k' : 0.35,  # W/mK
     }
-    step = 0.05
+    step = 0.1
     ime = 'Erazem Zver'
 
     izrisi_graf(podatki, step, ime)
 
 
 def izrisi_graf(podatki, step: float,ime):
+    import Izracun # Uvozimo datoteko Izračun.py
     import numpy as np
     import matplotlib.pyplot as plt
     import os
 
-    xarray = np.arange(0, podatki["L"]+step, step)
-    yarray = np.arange(0, podatki["W"]+step, step)
-    result = []
-
-    # Pravilno oblikovanje podatkov
-    for x in xarray:
-        y_ar = []
-        for y in yarray:
-            y_ar.append(Izracun.izracun(x, y, **podatki))
-        result.append(y_ar)
-    nparray = np.array(result)
-    X, Y = np.meshgrid(xarray, yarray)
+    #Izračun
+    x = np.arange(0, podatki["L"]+step, step)
+    y = np.arange(0, podatki["W"]+step, step)
+    X, Y = np.meshgrid(x, y)
+    result = Izracun.izracun(X, Y, **podatki)
 
     # Izrišemo graf
-    plt.contourf(Y, X, nparray, 8, cmap='plasma')  # cmap -> barvna lestvica
+    plt.contourf(X,Y, result, 8, cmap='plasma')  # cmap -> barvna lestvica
     plt.grid(alpha=0.2)  # alpha ->prosojnost črt
     plt.xlabel('X-os')
     plt.ylabel('Y-os')
     plt.title(ime)
     plt.colorbar()
-    cntr = plt.contour(Y, X, nparray, 8, colors='white', linewidths=.5)
+    cntr = plt.contour(X,Y, result, 8, colors='white', linewidths=.5)
     plt.clabel(cntr, inline_spacing=1, fmt='%.2f', fontsize=10)
 
     #Shranimo graf v sliko
     dir_path = os.path.dirname(os.path.realpath(__file__)) #Določimo mapo v kateri je main.py
     plt.savefig(dir_path+'/fig1.png') #Shranimo v to mapo
 
-
-entry()
+# Pokličemo funkcijo __main__, ki sproži program
+__main__()
