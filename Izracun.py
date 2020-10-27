2faf5767042825963b2c5cd823659d2df4a9fd17
ezvr <11723882+ezvr@users.noreply.github.com>
2020-10-27 16:51:16 +0100

    spremeljivke dal v vstopno funkcijo

--- Izracun.py
+++ Izracun.py
@@ -1,21 +1,13 @@
 import numpy as np
 
 
-def izracun(x: float = 0, y: float = 0):
+def izracun(x, y, q, L, T1, k, W, n):
     """Izračunaj vrednost temperature pri podanih podatkih.
 
     Keyword arguments:
     x -- premik v x smeri [m] (default 0.0)
     y -- premik v y smeri [m] (default 0.0)
     """
-
-
-    q = 15  # W/m2
-    L = 0.7  # m
-    n = 1  # /
-    T1 = 320  # K
-    k = 0.35  # W/mK
-    W = 0.7  # m
 
     A = 2 * q * L * (1 - (-1)**n)
     B = k * n**2 * np.pi**2 * np.cosh((n * np.pi * W) / L)
--- main.py
+++ main.py
@@ -3,27 +3,34 @@ import Izracun
 
 def entry():
     # TREBA NARDIT DA SE LAHKO VNESE PODATKE
-    L = 0.70  # m
-    W = 0.70  # m
+    podatki = {
+        'L' : 0.70,  # m
+        'W' : 0.70,  # m
+        'q' : 15,  # W/m2
+        'n': 1,  # /
+        'T1' : 320,  # K
+        'k' : 0.35,  # W/mK
+    }
     step = 0.05
+    ime = 'Erazem Zver'
 
-    izrisi_graf(L, W, step)
+    izrisi_graf(podatki, step, ime)
 
 
-def izrisi_graf(L: float, W: float, step: float):
+def izrisi_graf(podatki, step: float,ime):
     import numpy as np
     import matplotlib.pyplot as plt
     import os
 
-    xarray = np.arange(0, L+step, step)
-    yarray = np.arange(0, W+step, step)
+    xarray = np.arange(0, podatki["L"]+step, step)
+    yarray = np.arange(0, podatki["W"]+step, step)
     result = []
 
     # Pravilno oblikovanje podatkov
     for x in xarray:
         y_ar = []
         for y in yarray:
-            y_ar.append(Izracun.izracun(x, y))
+            y_ar.append(Izracun.izracun(x, y, **podatki))
         result.append(y_ar)
     nparray = np.array(result)
     X, Y = np.meshgrid(xarray, yarray)
@@ -33,7 +40,7 @@ def izrisi_graf(L: float, W: float, step: float):
     plt.grid(alpha=0.2)  # alpha ->prosojnost črt
     plt.xlabel('X-os')
     plt.ylabel('Y-os')
-    plt.title('Jaka Purič, 23202024')
+    plt.title(ime)
     plt.colorbar()
     dir_path = os.path.dirname(os.path.realpath(__file__))
     cntr = plt.contour(Y, X, nparray, 8, colors='white', linewidths=.5)
