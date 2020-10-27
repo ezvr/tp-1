db74e973dfe7c486c269c73987b256d0ae95dc73
ezvr <11723882+ezvr@users.noreply.github.com>
2020-10-27 17:10:28 +0100

    formatting

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def __main__():
     # TREBA NARDIT DA SE LAHKO VNESE PODATKE ?
     podatki = {
-        'L' : 1,  # m
-        'W' : 1,  # m
-        'q' : 15,  # W/m2
+        'L': 1,  # m
+        'W': 1,  # m
+        'q': 15,  # W/m2
         'n': 1,  # /
-        'T1' : 320,  # K
-        'k' : 0.35,  # W/mK
+        'T1': 320,  # K
+        'k': 0.35,  # W/mK
     }
     step = 0.1
     ime = 'Erazem Zver'
@@ -14,31 +14,33 @@ def __main__():
     izrisi_graf(podatki, step, ime)
 
 
-def izrisi_graf(podatki, step: float,ime):
-    import Izracun # Uvozimo datoteko Izračun.py
+def izrisi_graf(podatki, step: float, ime):
+    import Izracun  # Uvozimo datoteko Izračun.py
     import numpy as np
     import matplotlib.pyplot as plt
     import os
 
-    #Izračun
+    # Izračun
     x = np.arange(0, podatki["L"]+step, step)
     y = np.arange(0, podatki["W"]+step, step)
     X, Y = np.meshgrid(x, y)
     result = Izracun.izracun(X, Y, **podatki)
 
     # Izrišemo graf
-    plt.contourf(X,Y, result, 8, cmap='plasma')  # cmap -> barvna lestvica
+    plt.contourf(X, Y, result, 8, cmap='plasma')  # cmap -> barvna lestvica
     plt.grid(alpha=0.2)  # alpha ->prosojnost črt
     plt.xlabel('X-os')
     plt.ylabel('Y-os')
     plt.title(ime)
     plt.colorbar()
-    cntr = plt.contour(X,Y, result, 8, colors='white', linewidths=.5)
+    cntr = plt.contour(X, Y, result, 8, colors='white', linewidths=.5)
     plt.clabel(cntr, inline_spacing=1, fmt='%.2f', fontsize=10)
 
-    #Shranimo graf v sliko
-    dir_path = os.path.dirname(os.path.realpath(__file__)) #Določimo mapo v kateri je main.py
-    plt.savefig(dir_path+'/fig1.png') #Shranimo v to mapo
+    # Shranimo graf v sliko
+    # Določimo mapo v kateri je main.py
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    plt.savefig(dir_path+'/fig1.png')  # Shranimo v to mapo
+
 
 # Pokličemo funkcijo __main__, ki sproži program
 __main__()
